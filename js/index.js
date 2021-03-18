98b65ff0d11686ea515b3da9df993e1010453df5
chenxiaoxigg <chenxiaoxigg@163.com>
2021-03-18 10:17:03 +0800

    重写首页

--- js/index.js
+++ js/index.js
@@ -1,14 +1,30 @@
-var GRXX=1; //个人信息新增标志位 主要用来计数自增来更改每个对象的id，使得每个id不同
-var grxxList=[];// 给后台传参的集成数组
-$(async function(){
-   await $("#xingbie1").searchableSelect();//美化首个下拉框  
-}
-)
-function toPage(){
-   console.log('111')
-   // window.location.href='step1.html'
-}
-function toPage1(){
-   console.log('22222')
-   // window.location.href='step2.html'
+var footText=['1111','22222','33333','4444'];
+var footI=0;
+$(function(){
+  footTextChange()
+})
+// 底部字体循环
+function footTextChange(){
+  $.ajax({
+    type: "GET",
+    url: "js/words.txt",
+    dataType: "json",
+    success: function (data) {
+        console.log('data==========',data);
+    },
+    error: function(){
+     console.log('error')     
+    }
+});
+  if(footI<=footText.length){
+    setTimeout(()=>{
+    $("#foot_text").text(footText[footI])
+    footI++
+    footTextChange()
+    },1000000)
+  }else{
+    footI=0;
+    $("#foot_text").text(footText[footI])
+    footTextChange()
+  }
 }
