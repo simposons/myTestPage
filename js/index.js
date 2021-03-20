f992040a6f4b9b2e866f402d94d4f0f9573bf554
chenxiaoxigg <chenxiaoxigg@163.com>
2021-03-20 10:59:29 +0800

    首页

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-var colorChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];//颜色
+var colorChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']; //颜色
 var footI = 0;
-var time=100000;//延时时间
+var time = 100000; //延时时间
 $(function () {
   footTextChange()
+  initPage()
 })
 // 底部字体循环
 function footTextChange() {
@@ -12,41 +13,71 @@ function footTextChange() {
     dataType: "json",
     success: function (data) {
       console.log('data==========', data);
-      let color= generateMixed(6)
-      let backgroundColor= colorReverse(color)
-      if (footI===0) {
-        $("#foot_text").text(data[footI].text).css("color","#"+color).css("background-color","#"+backgroundColor)
-        footI++
-        footTextChange()
-      } else if (footI < data.length) {
-        setTimeout(() => {
-          $("#foot_text").text(data[footI].text).css("color","#"+color).css("background-color","#"+backgroundColor)
-          footI++
-          footTextChange()
-        }, time)
-      } else {
-        footI = 0;
-        $("#foot_text").text(data[footI].text).css("color","#"+color).css("background-color","#"+backgroundColor)
-        footTextChange()
-      }
+     footPage(data)
     },
     error: function () {
       console.log('error')
     }
   });
 }
+function footPage(footList){
+  let color = generateMixed(6)
+  let backgroundColor = colorReverse(color)
+  if (footI === 0) {
+    $("#foot_text").text(footList[footI].text).css("color", "#" + color).css("background-color", "#" + backgroundColor)
+    footI++
+    footPage(footList)
+  } else if (footI < footList.length) {
+    setTimeout(() => {
+      $("#foot_text").text(footList[footI].text).css("color", "#" + color).css("background-color", "#" + backgroundColor)
+      footI++
+      footPage(footList)
+    }, time)
+  } else {
+    footI = 0;
+    $("#foot_text").text(footList[footI].text).css("color", "#" + color).css("background-color", "#" + backgroundColor)
+    footPage(footList)
+  }
+}
 // 随机颜色
 function generateMixed(n) {
-    var res = "";
-    for (var i = 0; i < n; i++) {
-        var id = Math.ceil(Math.random() * 16);
-        res += colorChars[id];
-    }
-    return res;
+  var res = "";
+  for (var i = 0; i < n; i++) {
+    var id = Math.ceil(Math.random() * 16);
+    res += colorChars[id];
+  }
+  return res;
 }
 // 取颜色的反色
-function colorReverse(oldColor){
+function colorReverse(oldColor) {
   var oldColor = '0x' + oldColor.replace(/#/g, '');
   var str = '000000' + (0xFFFFFF - oldColor).toString(16);
- return str.substring(str.length - 6, str.length);
+  return str.substring(str.length - 6, str.length);
+}
+
+function initPage() {
+  $.ajax({
+    type: 'GET',
+    url: '../txt/block.json',
+    dataType: 'json',
+    success: function (data) {
+      console.log('data==========', data);
+      let dataList = data
+      if (dataList.length) {
+        let html = ''
+        $.each(dataList, function (index,item ) {
+          html = '<div class="block">' +
+            '<div class="block_content">' +
+            '</div>' +
+            '<div class="block_text"><a href="'+item.url+'">'+item.title+'</a></div>' +
+            '</div>'
+            console.log('html',html,item)
+          $('#blockPage').append(html)
+        })
+      }
+    },
+    error: function () {
+      console.log('error')
+    }
+  })
 }
