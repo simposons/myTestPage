4eabd86ef37ff32068c90c2821ed194c4cd08a6b
lupeng <chenxiaoxigg@163.com>
2021-08-03 11:12:51 +0800

    gengxin

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ $(function () {
 function footTextChange() {
   $.ajax({
     type: "GET",
-    url: "txt/words.json",
+    url: "../txt/words.json",
     dataType: "json",
     success: function (data) {
       console.log('data==========', data);
